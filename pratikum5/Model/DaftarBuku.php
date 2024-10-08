b5f398492fbd85e89ae5e3d27b4608cf2a4c5a8d
dewiyyyy <dewiluviani@gmail.com>
2024-10-08 12:02:58 +0700

    menambahkan tugas
    commit -m

---
+++ pratikum5/Model/DaftarBuku.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "Buku.php";
+
+class DaftarBuku{
+
+    public function getData(){
+        $daftar_buku = array(
+            new Buku('Belajar pemograman web','Emely W.','Informatika','2024'),
+            new Buku('Matematika Diskrit', 'Rinaldi munir','Andi Publisher','2020'),
+            new Buku('Kalkulus','Robert T.','Erlangga','2019'),
+            new Buku('Metodologi penelitian','Jamaes W','Pustaka UIN','2024'),
+
+        );
+
+        return $daftar_buku;
+
+    }
+
+    public function getKolomTabel(){
+        return array('No','Judul','Pengarang','Penerbit','Tahun');
+
+    }
+
+}
